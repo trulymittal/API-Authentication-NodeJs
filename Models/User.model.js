83bd9a1178f7d13c43449422ff0864e1ff25ef2e
Truly Mittal <trulymittal@gmail.com>
2020-06-29 16:06:18 +0530

    added a new document check in pre save

--- Models/User.model.js
+++ Models/User.model.js
@@ -17,9 +17,14 @@ const UserSchema = new Schema({
 
 UserSchema.pre('save', async function (next) {
   try {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(this.password, salt)
-    this.password = hashedPassword
+    /* 
+    Here first checking if the document is new by using a helper of mongoose .isNew, therefore, this.isNew is true if document is new else false, and we only want to hash the password if its a new document, else  it will again hash the password if you save the document again by making some changes in other fields incase your document contains other fields.
+    */
+    if (this.isNew) {
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(this.password, salt)
+      this.password = hashedPassword
+    }
     next()
   } catch (error) {
     next(error)
